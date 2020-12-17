463587f7b97dfaad94e33b66aaa97a9afa0be0c0
yoshidasa1 <a1304380@yahoo.co.jp>
2020-12-18 08:26:56 +0900

    Update pj_info.py

--- pj_info.py
+++ pj_info.py
@@ -1,15 +1,15 @@
-# 案件情報　毎回書き換え？
+# 案件情報　
 COMPANY = 'Z社様'
 BATCH = '2nd_2019'
 
 # 案件DailyアンケートURL
-DR_URL = "https://docs.google.com/spreadsheets/d/1mc68lxU-YnEiCy1eNSljSHFHAF6dUZ688PJnJbCU3Mc/edit#gid=2115676672"
-DR_SHEETKEY = "1mc68lxU-YnEiCy1eNSljSHFHAF6dUZ688PJnJbCU3Mc"
+DR_URL = ""
+DR_SHEETKEY = ""
 DR_SHEETRANGE = '回答一覧表!A3:N182'
 
 # 案件講師メールアドレス
-test = 'hommyo@alue.co.jp'
-prd1_mail = 'admin@alue-training.com'
+test = ''
+prd1_mail = ''
 
 to_email = prd1_mail  # 送りたい先のアドレス
 cc_email = test  # 送りたい先のアドレス
